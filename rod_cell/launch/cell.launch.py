8272abc6fee2eb6f9feefdf9198796c7f8097a41
Raini20 <popsel20@gmail.com>
2026-05-11 22:27:07 +0200

    feat: cell.launch.py fix, gz integration

--- rod_cell/launch/cell.launch.py
+++ rod_cell/launch/cell.launch.py
@@ -1,43 +1,36 @@
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
-from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch_ros.actions import Node
 
 def generate_launch_description():
+    arm_pkg = get_package_share_directory('robot_arm_6dof_assembly')
+    scara_pkg = get_package_share_directory('scara_4')
+    gz_package = get_package_share_directory('ros_gz_sim')
 
-    arm_urdf = os.path.join(
-        get_package_share_directory('robot_arm_6dof_assembly'),
-        'urdf', 'robot_arm_6dof_assembly.urdf')
+    with open(os.path.join(arm_pkg, 'urdf', 'robot_arm_6dof_assembly.urdf'), 'r') as f:
+        arm_description = f.read().replace('$(find robot_arm_6dof_assembly)', arm_pkg)
 
-    scara_urdf = os.path.join(
-        get_package_share_directory('scara_4'),
-        'urdf', 'SCARA_4.urdf')
+    with open(os.path.join(scara_pkg, 'urdf', 'SCARA_4.urdf'), 'r') as f:
+        scara_description = f.read().replace('$(find scara_4)', scara_pkg)
 
-    with open(arm_urdf, 'r') as f:
-        arm_description = f.read()
-
-    with open(scara_urdf, 'r') as f:
-        scara_description = f.read()
-
-    install_share = os.path.join(os.path.expanduser('~'), 'rod_ws', 'install', 'robot_arm_6dof_assembly', 'share') + ':' + os.path.join(os.path.expanduser('~'), 'rod_ws', 'install', 'scara_4', 'share')
-
-    gz_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('ros_gz_sim'),
-                         'launch', 'gz_sim.launch.py')
-        ),
-        launch_arguments={'gz_args': '-r empty.sdf'}.items()
+    gz_resource_path = (
+        os.path.join(os.path.expanduser('~'), 'rod_ws', 'install', 'robot_arm_6dof_assembly', 'share') + ':' +
+        os.path.join(os.path.expanduser('~'), 'rod_ws', 'install', 'scara_4', 'share')
     )
 
     return LaunchDescription([
-        SetEnvironmentVariable(
-            'GZ_SIM_RESOURCE_PATH',
-            install_share
-        ),
+        SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_resource_path),
+        SetEnvironmentVariable('GZ_SIM_SYSTEM_PLUGIN_PATH', '/opt/ros/jazzy/lib'),
 
-        gz_launch,
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(gz_package, 'launch', 'gz_sim.launch.py')
+            ),
+            launch_arguments={'gz_args': '-r empty.sdf'}.items()
+        ),
 
         Node(
             package='robot_state_publisher',
@@ -45,7 +38,6 @@ def generate_launch_description():
             name='arm_state_publisher',
             namespace='arm',
             parameters=[{'robot_description': arm_description}],
-            output='screen'
         ),
 
         Node(
@@ -54,7 +46,6 @@ def generate_launch_description():
             name='scara_state_publisher',
             namespace='scara',
             parameters=[{'robot_description': scara_description}],
-            output='screen'
         ),
 
         Node(
@@ -62,7 +53,6 @@ def generate_launch_description():
             executable='create',
             arguments=['-name', 'arm', '-topic', '/arm/robot_description',
                        '-x', '0.0', '-y', '0.0', '-z', '0.0'],
-            output='screen'
         ),
 
         Node(
@@ -70,6 +60,5 @@ def generate_launch_description():
             executable='create',
             arguments=['-name', 'scara', '-topic', '/scara/robot_description',
                        '-x', '1.5', '-y', '0.0', '-z', '0.0'],
-            output='screen'
         ),
     ])
